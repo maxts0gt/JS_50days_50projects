f10ee477ce9516a8a66f237f46bcdcc64d54d813
maxts0gt <bazarsadtsogtjargal@gmail.com>
2021-10-05 12:56:34 +0900

    created "Password Strength Checker Background" following tutorial

---
+++ D39 - Password Strength Background/script.js
@@ -0,0 +1,10 @@
+const password = document.getElementById('password');
+const background = document.getElementById('background');
+
+password.addEventListener('input', (e) => {
+	const input = e.target.value;
+	const length = input.length;
+
+	blueValue = 20 - length * 2;
+	background.style.filter = `blur(${blueValue}px)`;
+});
